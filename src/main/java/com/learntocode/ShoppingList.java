93d83fed45db262485cc706963e920d77ee86ff7
courtneyjohnson238 <courtneyjohnson238@gmail.com>
2023-04-12 11:35:06 -0400

    added new shopping list

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apples, oranges, avocados, almond milk, water, organic chicken, grapes");
+        System.out.println("dark chocolate, yogurt, cranberry, ginger");
+    }
+}
